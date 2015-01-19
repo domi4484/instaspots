31bb2e9789eab007629548ef48047f32626a6807
Damiano Lombardi <lombardi.damiano@gmail.com>
2015-01-20 00:35:54 +0100

    Imparando Symfony...

--- WebSite/Symfony/src/Instaspots/SpotsBundle/Controller/AdvertController.php
+++ WebSite/Symfony/src/Instaspots/SpotsBundle/Controller/AdvertController.php
@@ -9,9 +9,8 @@ class AdvertController extends Controller
 {
   public function indexAction()
   {
-    $content = $this->get('templating')->render('InstaspotsSpotsBundle:Advert:index.html.twig',
-                                                array('nome' => 'domi'));
-    return new Response($content);
+   return $this->render('InstaspotsSpotsBundle:Advert:index.html.twig', 
+                        array('listAdverts' => array()));
   }  
   
   public function viewAction($id)
@@ -24,6 +23,23 @@ class AdvertController extends Controller
     // qu'elle puisse l'afficher
 
     return new Response("Affichage de l'annonce d'id : ".$id);
+  }
+  
+  public function menuAction($limit)
+  {
+    // On fixe en dur une liste ici, bien entendu par la suite
+    // on la récupérera depuis la BDD !
+    $listAdverts = array(
+      array('id' => 2, 'title' => 'Recherche développeur Symfony2'),
+      array('id' => 5, 'title' => 'Mission de webmaster'),
+      array('id' => 9, 'title' => 'Offre de stage webdesigner')
+    );
+
+    return $this->render('InstaspotsSpotsBundle:Advert:menu.html.twig', array(
+      // Tout l'intérêt est ici : le contrôleur passe
+      // les variables nécessaires au template !
+      'listAdverts' => $listAdverts
+    ));
   }
 }
 
